8818bc95e2707ae8a9ec04d42bb0f92e201eb6a5
corcorans <corcorans13@gmail.com>
2013-11-26 08:22:46 -0600

    Added get_os function.

--- src/osutils.lua
+++ src/osutils.lua
@@ -1,7 +1,15 @@
 local osutils = {}
 
 function osutils:get_os()
+  local o = package.config:sub(1, 1)
 
+  if o == "\\" then
+    return "windows"
+  elseif o == "/" then
+    return "unix"
+  else
+    error("Unable to determine os.")
+  end
 end
 
 return osutils
